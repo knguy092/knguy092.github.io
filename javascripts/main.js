b3ca1934e375578c8f098fe3c048754efbee6cf2
Kevin Nguyen <knguy092@ucr.edu>
2017-01-14 15:53:40 -0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-var myName = "Codecademy";
+var myName = "Kevin Nguyen";
 
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
@@ -9,13 +9,6 @@ var letterColors = [red, orange, green, blue, purple];
 
 drawName(myName, letterColors);
 
-if(10 < 3)
-{
-    bubbleShape = 'square';
-}
-else
-{
-    bubbleShape = 'circle';
-}
+bubbleShape = 'circle';
 
 bounceBubbles();
